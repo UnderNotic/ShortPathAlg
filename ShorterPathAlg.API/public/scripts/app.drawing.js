1b7856473c9d3927e6356d59b8109ffd4a8f1227
undernotic <undernotic@gmail.com>
2015-11-29 23:01:03 +0100

    canvas impl part 1

--- ShorterPathAlg.API/public/scripts/app.drawing.js
+++ ShorterPathAlg.API/public/scripts/app.drawing.js
@@ -2,6 +2,9 @@
     var game = {};
 }
 
+game.thinLineThickness = 1;
+game.lines = [];
+
 game.drawCircle = function (x, y, radius) {
     var ctx = game.ctx;
     ctx.fillStyle = "whitesmoke";
@@ -10,3 +13,16 @@ game.drawCircle = function (x, y, radius) {
     ctx.closePath();
     ctx.fill();
 };
+
+game.drawLine = function (ctx, x1, y1, x2, y2, thickness) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = "#cfc";
+    ctx.stroke();
+}
+
+game.connectCircles = function(locations) {
+    //TODO connect locations from API response
+}
