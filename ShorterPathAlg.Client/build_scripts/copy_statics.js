858c00440723c4b8af130596062dec1144982e0d
undernotic <undernotic@gmail.com>
2016-01-07 22:28:48 +0100

    Open browser

--- ShorterPathAlg.Client/build_scripts/copy_statics.js
+++ ShorterPathAlg.Client/build_scripts/copy_statics.js
@@ -1,5 +1,3 @@
-"use strict"
-
 var fs = require("fs-extra");
 var path = require("path");
 
---
+++ ShorterPathAlg.Client/build_scripts/open_browser.js
@@ -0,0 +1,3 @@
+var open = require("open");
+open("http://localhost:8080/");
+console.log("hallo from browser");
